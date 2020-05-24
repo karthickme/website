099585045c6ef77f8231eb13edf294124c1018d9
Karthick Elangovan-Laptop <smekarthick2005@gmail.com>
2020-05-25 00:13:10 +0530

    Added changed db

--- site1/portfolio/portfolio/settings.py
+++ site1/portfolio/portfolio/settings.py
@@ -76,8 +76,12 @@ WSGI_APPLICATION = 'portfolio.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'hrsmxuug',
+        'USER': 'hrsmxuug',
+        'PASSWORD': 'l152dbFJKLwQ0RLg2x7t6hm7m7C_jExr',
+        'HOST': 'satao.db.elephantsql.com',
+        'PORT': '5432',
     }
 }
 
